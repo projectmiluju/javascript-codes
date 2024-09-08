a920b6efc302f95b6ce15f3c19f5183acebac164
projectmiluju <dnjsdyd0712@naver.com>
2024-09-08 09:19:30 +0900

    자바스크립트 배열과 문자열 내장 함수 실습

--- mission/script.js
+++ mission/script.js
@@ -1,10 +1,10 @@
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-for (let i = 0; i < array.length; i++){
-    if (array[i] % 2 === 0){
-        console.log(array[i]);
-    }
-}
+// for (let i = 0; i < array.length; i++){
+//     if (array[i] % 2 === 0){
+//         console.log(array[i]);
+//     }
+// }
 
 
 
@@ -14,3 +14,27 @@ for (let i = 0; i < array.length; i++){
 //         console.log(value);
 //     }
 // }
+
+
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+myArray.push(11, 12);
+console.log(myArray);
+
+myArray.pop();
+console.log(myArray);
+myArray.pop();
+console.log(myArray);
+
+console.log(myArray.slice(0,5));
+
+myArray.splice(1,2,'a','b');
+console.log(myArray);
+
+
+const sentence = "Hello My Name is Yong."
+
+console.log(sentence.toUpperCase());
+console.log(sentence.toLowerCase());
+
+console.log(sentence.substring(0,5));
